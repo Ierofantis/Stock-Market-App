eb34efa3a7ab8c934b959f485d83b12d51f69fc4
Ierofantis <evil69669@gmail.com>
2017-04-15 16:45:03 +0300

    Refactoring

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,5 @@
 angular.module('Test', ['ionic',
+<<<<<<< HEAD
     'Test.controllers',
     'angular-cache',
     'ngStorage',
@@ -54,4 +55,61 @@ angular.module('Test', ['ionic',
     });
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/app/my-stocks');
+=======
+  'Test.controllers',
+  'angular-cache',
+  'ngStorage',
+  'Test.services',
+  'Test.filters',
+  'Test.directives'
+  ])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });  
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.myStocks', {
+    url: '/my-stocks',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/my-stocks.html',
+        controller: 'MyStocksCtrl'
+      }
+    }
+  })
+
+  .state('app.single', {
+    url: '/:stockTicker',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/stock.html',
+        controller: 'StockCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/my-stocks');
+>>>>>>> afca636919232fb151c9a7dae89796d4d07f49bf
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,5 @@
 angular.module('Test.services', [])
+<<<<<<< HEAD
     // create a new factory
     .factory('StorageService', function($localStorage) {
         $localStorage = $localStorage.$default({
@@ -21,11 +22,36 @@ angular.module('Test.services', [])
             remove: _remove
         };
     })
+=======
+// create a new factory
+.factory ('StorageService', function ($localStorage) {
+    $localStorage = $localStorage.$default({
+    things: []
+});
+
+var _getAll = function () {
+  return $localStorage.things;
+};
+var _add = function (thing) {
+  $localStorage.things.push(thing);  
+}
+var _remove = function (thing) {
+  $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
+}
+
+return {
+    getAll: _getAll,
+    add: _add,
+    remove: _remove
+  };
+})
+>>>>>>> afca636919232fb151c9a7dae89796d4d07f49bf
 
 .factory('stockDataService', function($q, $http) {
 
     var getPriceData = function(ticker) {
         var deferred = $q.defer(),
+<<<<<<< HEAD
             url = 'http://finance.yahoo.com/webservice/v1/symbols/' + ticker + '/quote?format=json&view=detail'
 
         $http.get(url)
@@ -37,9 +63,30 @@ angular.module('Test.services', [])
             .error(function() {
                 deferred.reject();
             })
+=======
+        url = 'http://finance.yahoo.com/webservice/v1/symbols/'+ticker+'/quote?format=json&view=detail'
+
+        $http.get(url)
+        .success(function(jsonData) {
+            console.log(jsonData.list);
+            var jsonData = jsonData.list.resources[0].resource.fields
+            deferred.resolve(jsonData);
+        })
+        .error(function() {
+            deferred.reject();
+        })
+>>>>>>> afca636919232fb151c9a7dae89796d4d07f49bf
         return deferred.promise;
     };
     return {
         getPriceData: getPriceData
     }
 });
+<<<<<<< HEAD
+=======
+
+
+
+
+
+>>>>>>> afca636919232fb151c9a7dae89796d4d07f49bf
