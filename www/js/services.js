3fcf08b68f7ec5b09b15abafcb88e583717b1d70
Theodore P <evil69669@gmail.com>
2017-10-01 17:34:58 +0300

    duplicate delete

--- www/js/services.js
+++ www/js/services.js
@@ -22,30 +22,6 @@ angular.module('Test.services', [])
         };
     })
 
-// create a new factory
-.factory('StorageService', function($localStorage) {
-    $localStorage = $localStorage.$default({
-        things: []
-    });
-
-    var _getAll = function() {
-        return $localStorage.things;
-    };
-    var _add = function(thing) {
-        $localStorage.things.push(thing);
-    }
-    var _remove = function(thing) {
-        $localStorage.things.splice($localStorage.things.indexOf(thing), 1);
-    }
-
-    return {
-        getAll: _getAll,
-        add: _add,
-        remove: _remove
-    };
-})
-
-
 .factory('stockDataService', function($q, $http) {
 
     var getPriceData = function(ticker) {
